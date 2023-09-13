6c95fe10e94f250ed9b990d1619a6ed4f11a8c4e
Motalleb Fallahnehzad <fmotalleb@gmail.com>
2023-09-13 16:27:26 +0330

    [Minor]

--- cord-junction.go
+++ cord-junction.go
@@ -10,19 +10,19 @@ import (
 
 func main() {
 	e := echo.New()
+
 	routerGroup := e.Group("/")
 	routerGroup.Use(func(handlerFunc echo.HandlerFunc) echo.HandlerFunc {
 		return func(context echo.Context) error {
 			req := context.Request()
 			res := context.Response().Writer
 			url, _ := url.Parse(fmt.Sprintf("%s://%s%s", "https", req.Host, req.RequestURI))
-
-			println(url.Scheme)
 			url.Scheme = "https"
 			proxy := httputil.NewSingleHostReverseProxy(url)
+
 			//trim reverseProxyRoutePrefix
-			path := req.URL.Path
-			req.URL.Path = path
+			// path := req.URL.Path
+			// req.URL.Path = path
 
 			// ServeHttp is non blocking and uses a go routine under the hood
 			proxy.ServeHTTP(res, req)
